607b8667ecd6283313c49583f795b3c5b2936b3d
Landing_page_anime <i.jackreacher@mail.ru>
2021-12-11 21:39:12 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -48,8 +48,8 @@ $(function () {
             blockId = $this.data('scroll'),
             blockOffset = $(blockId).offset().top;
 
-            $("#nav a").removeClass("active");
-            $this.addClass("active");
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
 
         $("html, body").animate({
             scrollTop: blockOffset
@@ -57,22 +57,22 @@ $(function () {
     });
 
     // menu nav toggle
-    $("#nav_toggle").on("click", function(event) {
+    $("#nav_toggle").on("click", function (event) {
         event.preventDefault();
 
         $(this).toggleClass("active");
-       
-      
+
+
         $("#nav").toggleClass("active");
     });
 
-//   collapse
+    //   collapse
 
-    $("[data-collapse]").on("click", function(event) {
-    event.preventDefault();
+    $("[data-collapse]").on("click", function (event) {
+        event.preventDefault();
 
-    let $this = $(this),
-        blockId = $this.data('collapse');
+        let $this = $(this),
+            blockId = $this.data('collapse');
 
         $this.toggleClass("active")
         $(blockId).slideToggle();//конфликт с классами
@@ -80,3 +80,100 @@ $(function () {
 
 
 });
+
+
+
+
+/*********************************************************** */
+
+let deleteSlides = document.querySelectorAll('.section__header ');
+
+deleteSlides.forEach(item => {
+    item.remove();
+});
+
+
+
+
+
+
+/************************************************ */
+
+
+
+// const personalMovieDB = {
+//     count: 0,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+//     start: function () {
+//         personalMovieDB.count = +prompt("Сколько фильмов вы уже просмотрели?", '');
+
+//         while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+//             personalMovieDB.count = +prompt("Сколько фильмов вы уже просмотрели?", '');
+//         }
+//     },
+
+//     rememberMyFilms: function () {
+//         for (let i = 0; i < 2; i++) {
+//             let a = prompt("Один из просмотренных фильмов", ''),
+//                 b = prompt("На сколько оцените его", '');
+
+//             if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//                 personalMovieDB.movies[a] = b;
+//                 console.log('done');
+//             } else {
+//                 console.log('error');
+//                 i--;
+//             }
+//         }
+//     },
+
+//     detectPersonalLevel: function () {
+//         if (personalMovieDB.count < 10) {
+//             console.log('Просмотрено мало фильмов');
+//         } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//             console.log('Вы класический зритель');
+//         } else if (personalMovieDB.count >= 30) {
+//             console.log('Вы киноман');
+//         } else {
+//             console.log('Произошла ошибка');
+//         }
+
+//     },
+//     showMyDB: function (hidden) {
+//         if (!hidden) {
+//             console.log(personalMovieDB);
+//         }
+//     },
+//     toggleVisivleMyDB: function () {
+//         if (personalMovieDB.privat) {
+//             personalMovieDB.privat = false;
+//         } else {
+//             personalMovieDB.privat = true;
+//         }
+//     },
+//     writeYourGenres: function () {
+//         for (let i = 1; i <= 3; i++) {
+//             let genre = prompt(`Ваш любимы жанр под номером ${i}`);
+
+//             if (genre == '' || genre == null) {
+//                 console.log('вы ввели некорректные данные или не ввели их вовсе');
+//                 i--;
+//             } else {
+//                 personalMovieDB.genres[i - 1] = genre.toLowerCase();;
+//                 personalMovieDB.genres.sort();
+//             }
+//         }
+
+//         personalMovieDB.genres.forEach((item, i) => {
+//             console.log(`Любимый жанр ${i + 1} - это ${item}`);
+
+//         });
+//     }
+
+
+// };
+
+
